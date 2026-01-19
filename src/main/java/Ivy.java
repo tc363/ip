15206d35e7ffd5f76267e1c17d58ef3af91b4b16
tricia <e1355062@u.nus.edu>
2026-01-19 11:00:20 +0800

    Level-1: Format output with indentation

--- src/main/java/Ivy.java
+++ src/main/java/Ivy.java
@@ -3,15 +3,16 @@ import java.util.Scanner;
 public class Ivy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! I'm Ivy.\nWhat can I do for you?");
+        System.out.println("    Hello! I'm Ivy.");
+        System.out.println("    What can I do for you?");
 
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("    Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(input);
+            System.out.println("    " + input);
         }
 
         scanner.close();
