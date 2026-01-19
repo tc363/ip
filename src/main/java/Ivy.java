0a802af9f56673a684395e501ae5e21c56b339e8
tricia <e1355062@u.nus.edu>
2026-01-19 10:51:40 +0800

    Level-1: Add echo feature and exit on bye

--- src/main/java/Ivy.java
+++ src/main/java/Ivy.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Ivy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello! I'm Ivy.\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
