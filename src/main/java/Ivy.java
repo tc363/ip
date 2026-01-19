0f55c5707198948f199ffab08d42305561f2890c
tricia <e1355062@u.nus.edu>
2026-01-19 15:50:08 +0800

    Level-4: Add support for Todo, Deadline and Event tasks

--- src/main/java/Ivy.java
+++ src/main/java/Ivy.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
 public class Ivy {
+    private static final int MAX_TASKS = 100;
+    private static Task[] tasks = new Task[MAX_TASKS];
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
 
         System.out.println("    Hello! I'm Ivy.");
         System.out.println("    What can I do for you?");
@@ -13,38 +14,60 @@ public class Ivy {
         while (true) {
             String input = scanner.nextLine();
             String[] parts = input.split(" ", 2);
+            String command = parts[0];
+            String arg = parts.length > 1 ? parts[1] : "";
 
             if (input.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equals("list")) {
+            } else if (command.equals("list")) {
                 System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println("    " + (i + 1) + ". " + tasks[i]);
                 }
-            } else if (parts[0].equals("mark")) {
-                int index = Integer.parseInt(parts[1]) - 1;
+            } else if (command.equals("mark")) {
+                int index = Integer.parseInt(arg) - 1;
                 if (index < taskCount && index >= 0) {
                     tasks[index].markAsDone();
                     System.out.println("    Nice! I've marked this task as done:");
                     System.out.println("      " + tasks[index].toString());
                 }
-            } else if (parts[0].equals("unmark")) {
-                int index = Integer.parseInt(parts[1]) - 1;
+            } else if (command.equals("unmark")) {
+                int index = Integer.parseInt(arg) - 1;
                 if (index < taskCount && index >= 0) {
                     tasks[index].markAsNotDone();
                     System.out.println("    OK, I've marked this task as not done yet:");
                     System.out.println("      " + tasks[index].toString());
                 }
-            } else {
+            } else if (command.equals("todo")) {
                 if (taskCount < tasks.length) {
-                    tasks[taskCount] = new Task(input);
+                    tasks[taskCount] = new Todo(arg);
+                    printAdded(tasks[taskCount]);
                     taskCount++;
-                    System.out.println("    added: " + input);
+                }
+            } else if (command.equals("deadline")) {
+                if (taskCount < tasks.length) {
+                    String[] deadlineParts = input.split(" /by ", 2);
+                    tasks[taskCount] = new Deadline(deadlineParts[0], deadlineParts[1]);
+                    printAdded(tasks[taskCount]);
+                    taskCount++;
+                }
+            } else if (command.equals("event")) {
+                if (taskCount < tasks.length) {
+                    String[] eventParts = input.split(" /from | /to ", 3);
+                    tasks[taskCount] = new Event(eventParts[0], eventParts[1], eventParts[2]);
+                    printAdded(tasks[taskCount]);
+                    taskCount++;
                 }
             }
         }
 
         scanner.close();
+    }
+
+    public static void printAdded(Task task) {
+        System.out.println("    Got it. I've added this task:");
+        System.out.println("      " + task);
+        System.out.println("    Now you have " + (taskCount + 1) + " tasks in the list.");
     }
 }
