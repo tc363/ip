070e0b7e3f782c902a1e9382590ecaa330224f4e
tricia <e1355062@u.nus.edu>
2026-01-19 11:44:26 +0800

    Level-2: Add task storage and list command

--- src/main/java/Ivy.java
+++ src/main/java/Ivy.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Ivy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("    Hello! I'm Ivy.");
         System.out.println("    What can I do for you?");
 
@@ -11,8 +15,17 @@ public class Ivy {
             if (input.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("    " + (i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                if (taskCount < tasks.length) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("    added: " + input);
+                }
             }
-            System.out.println("    " + input);
         }
 
         scanner.close();
