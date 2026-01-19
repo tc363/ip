b59fb0691dc2b52c835abc4ca78b02d70099d522
tricia <e1355062@u.nus.edu>
2026-01-19 13:19:42 +0800

    Level-3: Update Ivy to support mark/unmark tasks

--- src/main/java/Ivy.java
+++ src/main/java/Ivy.java
@@ -4,7 +4,7 @@ public class Ivy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("    Hello! I'm Ivy.");
@@ -12,16 +12,33 @@ public class Ivy {
 
         while (true) {
             String input = scanner.nextLine();
+            String[] parts = input.split(" ", 2);
+
             if (input.equals("bye")) {
                 System.out.println("    Bye. Hope to see you again soon!");
                 break;
             } else if (input.equals("list")) {
+                System.out.println("    Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println("    " + (i + 1) + ". " + tasks[i]);
                 }
+            } else if (parts[0].equals("mark")) {
+                int index = Integer.parseInt(parts[1]) - 1;
+                if (index < taskCount && index >= 0) {
+                    tasks[index].markAsDone();
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("      " + tasks[index].toString());
+                }
+            } else if (parts[0].equals("unmark")) {
+                int index = Integer.parseInt(parts[1]) - 1;
+                if (index < taskCount && index >= 0) {
+                    tasks[index].markAsNotDone();
+                    System.out.println("    OK, I've marked this task as not done yet:");
+                    System.out.println("      " + tasks[index].toString());
+                }
             } else {
                 if (taskCount < tasks.length) {
-                    tasks[taskCount] = input;
+                    tasks[taskCount] = new Task(input);
                     taskCount++;
                     System.out.println("    added: " + input);
                 }
