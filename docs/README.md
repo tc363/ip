a2b640e890907577a92ce94eb2efc9eab0f63cae
tricia <e1355062@u.nus.edu>
2026-01-19 09:49:16 +0800

    Rename Duke to Ivy and add greeting and exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Ivy User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Ivy.java
@@ -0,0 +1,6 @@
+public class Ivy {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Ivy.\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
