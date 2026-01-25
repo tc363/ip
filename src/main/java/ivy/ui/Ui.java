e4823063af423e5caeeba7193bdb205b8d51f8a3
tricia <e1355062@u.nus.edu>
2026-01-25 22:17:14 +0800

    Add JavaDoc comments to Ui class

--- src/main/java/ivy/ui/Ui.java
+++ src/main/java/ivy/ui/Ui.java
@@ -5,26 +5,48 @@ import ivy.task.TaskList;
 
 import java.util.Scanner;
 
+/**
+ * Handles all user interactions including reading input and displaying messages.
+ */
 public class Ui {
     private final Scanner scanner;
 
+    /**
+     * Constructs a {@code Ui} object and initializes the input scanner.
+     */
     public Ui() {
         scanner = new Scanner(System.in);
     }
 
+    /**
+     * Shows welcome message when program starts.
+     */
     public void showWelcome() {
         System.out.println("    Hello! I'm ivy.Ivy.");
         System.out.println("    What can I do for you?");
     }
 
+    /**
+     * Shows farewell message when program ends.
+     */
     public void showFarewell() {
         System.out.println("    Bye. Hope to see you again soon!");
     }
 
+    /**
+     * Reads a line of user input.
+     *
+     * @return User's input as a string.
+     */
     public String readCommand() {
         return scanner.nextLine();
     }
 
+    /**
+     * Displays full list of tasks.
+     *
+     * @param tasks {@code TaskList} to display.
+     */
     public void showTaskList(TaskList tasks) {
         System.out.println("    Here are the tasks in your list:");
         for (int i = 0; i < tasks.getTaskCount(); i++) {
@@ -32,32 +54,62 @@ public class Ui {
         }
     }
 
+    /**
+     * Displays a message when a task has been added.
+     *
+     * @param task Task added.
+     * @param totalTasks Total number of tasks in list.
+     */
     public void showTaskAdded(Task task, int totalTasks) {
         System.out.println("    Got it. I've added this task:");
         System.out.println("      " + task);
         System.out.println("    Now you have " + totalTasks + " tasks in the list.");
     }
 
+    /**
+     * Displays a message when a task has been marked as done.
+     *
+     * @param task Task marked as done.
+     */
     public void showTaskMarked(Task task) {
         System.out.println("    Nice! I've marked this task as done:");
         System.out.println("      " + task);
     }
 
+    /**
+     * Displays a message when a task has been unmarked.
+     *
+     * @param task Task unmarked.
+     */
     public void showTaskUnmarked(Task task) {
         System.out.println("    OK, I've marked this task as not done yet:");
         System.out.println("      " + task);
     }
 
+    /**
+     * Displays a message when a task has been deleted.
+     *
+     * @param task Task deleted.
+     * @param totalTasks Total number of tasks in list.
+     */
     public void showTaskDeleted(Task task, int totalTasks) {
         System.out.println("    Noted. I've removed this task:");
         System.out.println("      " + task);
         System.out.println("    Now you have " + totalTasks + " tasks in the list.");
     }
 
+    /**
+     * Displays an error message to the user.
+     *
+     * @param message Error message.
+     */
     public void showError(String message) {
         System.out.println("    " + message);
     }
 
+    /**
+     * Closes the input scanner. Should be called when program exits.
+     */
     public void exit() {
         scanner.close();
     }
