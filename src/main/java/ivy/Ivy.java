1e88a4461eb4937b6a08f40e96312035b40c147e
tricia <e1355062@u.nus.edu>
2026-01-30 22:33:43 +0800

    Add user interaction and input handling to GUI

--- src/main/java/ivy/Ivy.java
+++ src/main/java/ivy/Ivy.java
@@ -17,15 +17,14 @@ public class Ivy {
     private TaskList tasks;
     private Storage storage;
     private Ui ui;
+    private String FILE_PATH = "data/ivy.txt";
 
     /**
      * Constructs an {@code Ivy} object with given storage file path.
-     *
-     * @param filePath Path to file used for storing tasks.
      */
-    public Ivy(String filePath) {
+    public Ivy() {
         ui = new Ui();
-        storage = new Storage(filePath);
+        storage = new Storage(FILE_PATH);
         tasks = new TaskList(storage.loadTasks());
     }
 
@@ -57,7 +56,7 @@ public class Ivy {
     }
 
     public static void main(String[] args) {
-        new Ivy("data/ivy.txt").run();
+        new Ivy().run();
     }
 
     /**
@@ -155,5 +154,12 @@ public class Ivy {
     private void findTask(String keyword) {
         TaskList matches = tasks.findTasks(keyword);
         ui.showMatchingTasks(matches);
+    }
+
+    /**
+     * Generates a response for the user's chat message.
+     */
+    public String getResponse(String input) {
+        return "Duke heard: " + input;
     }
 }
--- src/main/java/ivy/Main.java
+++ src/main/java/ivy/Main.java
@@ -20,6 +20,8 @@ public class Main extends Application {
     private Scene scene;
     private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.jpg"));
     private Image ivyImage = new Image(this.getClass().getResourceAsStream("/images/ivy.jpg"));
+    private Ivy ivy = new Ivy();
+
 
     @Override
     public void start(Stage stage) {
@@ -32,9 +34,6 @@ public class Main extends Application {
         userInput = new TextField();
         sendButton = new Button("Send");
 
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
-
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
 
@@ -45,7 +44,7 @@ public class Main extends Application {
 
         //Formatting the window to look as expected
 
-        stage.setTitle("Duke");
+        stage.setTitle("Ivy");
         stage.setResizable(false);
         stage.setMinHeight(600.0);
         stage.setMinWidth(400.0);
@@ -72,5 +71,32 @@ public class Main extends Application {
 
         AnchorPane.setLeftAnchor(userInput, 1.0);
         AnchorPane.setBottomAnchor(userInput, 1.0);
+
+        //Handling user input
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
+
+        //Scroll down to the end every time dialogContainer's height changes.
+        dialogContainer.heightProperty().addListener((observable) -> scrollPane.setVvalue(1.0));
     }
+
+    /**
+     * Creates a dialog box containing user input, and appends it to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String ivyText = ivy.getResponse(userInput.getText());
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getIvyDialog(ivyText, ivyImage)
+        );
+        userInput.clear();
+    }
+
 }
